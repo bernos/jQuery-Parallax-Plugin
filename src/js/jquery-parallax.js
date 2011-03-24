0b0937ade492823e8d570703c484ff4f563920d0
Brendan McMahon <bernos@gmail.com>
2011-03-25 00:31:18 +1100

    started on basic scroll routines

--- src/js/jquery-parallax.js
+++ src/js/jquery-parallax.js
@@ -4,14 +4,38 @@
     init: function(options) {
       
       var settings = {
-        
+        width:  this.width(),
+        height: this.height()
       };
 
       return this.each(function(){
         if (options) {
           $.extend(settings, options);  
         }
+
+        $(this).css('position', 'relative');
+        $(this).width(settings.width);
+        $(this).height(settings.height);
+
+        $(this).append('<div class="background" style="position:absolute;top:0;left:0;z-index:1;"></div><div class="foreground" style="position:absolute;top:0;left:0;z-index:2;"></div>');
       });
+    },
+
+    scrollTo: function($el, direction) {
+
+
+      $('.foreground', this).append($el.css({
+        'top': '0px',
+        'left' : (this.width() - $('.foreground', this).position().left) + 'px',
+        'position' : 'absolute'
+      }));
+
+      if (direction == 'left') {
+        $fg = $('.foreground', this);
+
+        console.debug('move from ' + $fg.position());
+        $fg.animate({left:$fg.position().left - this.width()})
+      }
     }
   };
   
